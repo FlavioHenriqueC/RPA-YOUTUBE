f62f15b21940c1e376d52fec342bdff7bfce404f
FlavioHenriqueC <flaviohenrique2355@gmail.com>
2026-08-30 11:30:05 -0300

    modificando

--- main.py
+++ main.py
@@ -24,5 +24,5 @@ def baixar_audio_youtube(url_do_video, pasta_destino=r'C:\Users\flavi\OneDrive\�
         print("Download e conversão concluídos!")
 
 if __name__ == '__main__':
-    url = "-"
+    url = "--"
     baixar_audio_youtube(url)
